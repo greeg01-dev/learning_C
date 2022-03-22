8f9d20ffdc1412743d2ad2b4d491d68f9f4cd6f4
greeg01 <greeg01-dev>
2022-03-22 20:19:55 +0900

    feat: make input_output.c file
    learn scanf function to take some input values and printf function to print a value

---
+++ learning/input_output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int m, n; // variables to take input values
+
+    scanf("%d %d", &m, &n); // input value
+
+    printf("%d", m + n); // print the sum of value
+
+    return 0;
+}
