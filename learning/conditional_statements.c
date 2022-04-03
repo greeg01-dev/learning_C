b9dd85d40c0f1fbade258d9bb0a5efbe4be7e1c8
greeg01-dev <dev.greeg01@gmail.com>
2022-04-03 21:22:32 +0900

    feat: add ternary operater and declare three variables at the same time

--- learning/conditional_statements.c
+++ learning/conditional_statements.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	int a = 1;
+	// declare variables
+	int a = 0, b = 1, c = 2;
 
 	// if..else statement
 	printf("if..else statement \n");
@@ -20,16 +21,24 @@ int main() {
 	// switch..case statement
 	printf("switch..case statement \n");
 
-	switch (a) {
+	switch (b) {
 		case 0:
-			printf("a is 0");
+			printf("b is 0");
 			break;
 		case 1:
-			printf("a is 1");
+			printf("b is 1");
 			break;
 		default:
-			printf("a is neither 0 nor 1");
+			printf("b is neither 0 nor 1");
 	}
-	
+
+	// line break
+	printf("\n\n");
+
+	// ternary operator
+	printf("ternary operator \n");
+
+	printf("c is %s", c == 0? "0": c == 1? "1": "neither 0 nor 1");
+
 	return 0;
 }
